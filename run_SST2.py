57c0b44b122853af3a55c2591213f7e233a1ec70
wll <62608056+wll-best@users.noreply.github.com>
2020-12-30 11:06:55 +0800

    Add files via upload

---
+++ run_SST2.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+from main import main
+
+
+if __name__ == "__main__":
+
+    model_name = "BertOrigin"
+    label_list = ['0', '1']
+    data_dir = "./SST-2"
+    output_dir = ".sst_output/" 
+    cache_dir = ".sst_cache/"
+    log_dir = ".sst_log/" 
+
+    # bert-base
+    bert_vocab_file = "./bert-base-uncased-vocab.txt"
+    bert_model_dir = "./bert-base-uncased"
+
+    # # bert-large
+    # bert_vocab_file = "/search/hadoop02/suanfa/songyingxin/pytorch_Bert/bert-large-uncased-vocab.txt"
+    # bert_model_dir = "/search/hadoop02/suanfa/songyingxin/pytorch_Bert/bert-large-uncased"
+
+    if model_name == "BertOrigin":
+        from BertOrigin import args
+
+    elif model_name == "BertCNN":
+        from BertCNN import args
+
+    elif model_name == 'BertLSTM':
+        from BertLSTM import args
+
+    elif model_name == "BertATT":
+        from BertATT import args
+
+    elif model_name == "BertRCNN":
+        from BertRCNN import args
+
+    elif model_name == "BertCNNPlus":
+        from BertCNNPlus import args
+    
+    elif model_name == "BertDPCNN":
+        from BertDPCNN import args
+
+    config = args.get_args(data_dir, output_dir, cache_dir,
+                           bert_vocab_file, bert_model_dir, log_dir)
+
+    main(config, config.save_name, label_list)
+
+    #命令行语句
+    #python3 run_SST2.py --max_seq_length=65 --num_train_epochs=5.0 --do_train --gradient_accumulation_steps=8 --print_step=100  # train and test
+    #python3 run_SST2.py --max_seq_length=65   # test
+        
+
